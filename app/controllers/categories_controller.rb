97e7d20f8fb7261a09163dae8949ed3db886ca55
ildus523 <ildus523@mail.ru>
2011-09-09 08:21:52 +0400

    Cart is modified

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
   def show
     @cart = current_cart
-    @products = Category.find(params[:id]).products
+    @products = Category.find(params[:id]).products.paginate(:page => params[:page], :per_page => 6)
+
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @category }
